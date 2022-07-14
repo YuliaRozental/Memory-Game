91f8d9ebc1fd491cab2615ca2f688349afd94a32
Yulia <yuyarozental@gmail.com>
2022-07-14 16:12:23 -0400

    new commit

--- README.md
+++ README.md
@@ -2,27 +2,26 @@
 
 Project Description
 
-I want to create a memory game. The game is about recalling the correct sequence and repeat it. Player needs to recall the correct sequence – the order in which the squares illuminate and get a score. Each level gets more difficult (longer sequences, faster flashing) making it harder to memorize or recollect the correct pattern.
+I want to create a memory game. The object of the game is to repeat a series of random tile clicks created by the game. After each round, the sequence becomes progressively longer and more complex (longer sequences, faster flashing) which makes it harder to remember. Player needs to recall the correct sequence – the order in which the squares illuminate and get a score. 
 
 Game Logic
 
-1. Let’s say there are 16 squares in the grid (4x4)
+1. Let’s say there are 9 squares in the grid (3x3)
 2. The user is confronted with a grid of empty squares
-3. When user click the 'start' button a sequence is “flashed” to the user, so the squares illuminate in turn in a particular order or sequence
-5. The first round could start with one square illuminates and then the number of flashing cards will be progressively increasing from round to round (2, 3, 4, etc.)
-7. The user must correctly replicate this sequence in order to pass the level
-8. If player was able to recall the sequence correctly the score will be incresed by 1 otherwise the score won't change. 
+3. A round begins when the user clicks the 'start' button. Game activates one or more tiles in a random order and ends when the player reproduces the order by pressing the tiles. On the next round, the number of tiles in the sequence increases by one.
+4. The user must correctly replicate this sequence in order to pass the level
+5. If player was able to recall the sequence correctly the score will be incresed by 1 otherwise the score won't change. 
 
 
 Deliverables
 
 MVP Criteria
-1. Player must be able to complete the whole 16 square flashing sequence 
+1. Player must be able to complete the whole 9 square flashing sequence 
 2. Game must alert the player if he didn't repeat the sequence correctly
 3. Cards must illumination and move more quickly. Making it harder to memorize or recollect the correct pattern.
 3. Game must alert the player about the score after each round completed
 
 Post-MVP Plans
 
-1. Maybe adding more squares
-2. Maybe making a few parrallel sequenses with different colors of the squares 
+1. Add more squares (4X4)
+2. Cnahge the style
---
+++ script.js
@@ -0,0 +1,112 @@
+//array to keep track of the original sequence of tile clicks and a second array for the player sequence
+let sequence = [];
+let humanSequence = [];
+
+//to keep track of the numbers of rounds that have been played
+let level = 0;
+
+const startButton = document.querySelector('.js-start');
+const info = document.querySelector('.js-info');
+const heading = document.querySelector('.js-heading');
+const boardContainer = document.querySelector('.js-container');
+
+function resetGame(text) {
+    alert(text);
+    sequence = [];
+    humanSequence = [];
+    level = 0;
+    startButton.classList.remove('hidden');
+    heading.textContent = 'Memory Game';
+    info.classList.add('hidden');
+    boardContainer.classList.add('unclickable');
+}
+  
+function humanTurn(level) {
+    boardContainer.classList.remove('unclickable');
+    info.textContent = `Your turn: ${level} Tap${level > 1 ? 's' : ''}`;
+}
+
+function activateCard(color) {
+    const card = document.querySelector(`[data-card='${color}']`);
+
+    card.classList.add('activated');
+
+    setTimeout(() => {
+    card.classList.remove('activated');
+    }, 300);
+}
+  
+function playRound(nextSequence) {
+    nextSequence.forEach((color, index) => {
+      setTimeout(() => {
+        activateCard(color);
+      }, (index + 1) * 600);
+    });
+}
+
+function nextStep() {
+    const cards = ['pink', 'teal', 'orange', 'yellow', 'red', 'blue'];
+    const random = cards[Math.floor(Math.random() * cards.length)];
+    return random;
+}
+
+function nextRound() {
+    level += 1;
+
+    boardContainer.classList.add('unclickable');
+    info.textContent = 'Wait for the computer';
+    heading.textContent = `Level ${level} of 15`;
+
+    const nextSequence = [...sequence];
+    nextSequence.push(nextStep());
+    playRound(nextSequence);
+
+    sequence = [...nextSequence];
+    setTimeout(() => {
+    humanTurn(level);
+  }, level * 600 + 1000);
+}
+
+
+function handleClick(tile) {
+    const index = humanSequence.push(tile) - 1;
+  
+    const remainingTaps = sequence.length - humanSequence.length;
+  
+    if (humanSequence[index] !== sequence[index]) {
+      resetGame('Oops! Game over, you pressed the wrong tile');
+      return;
+    }
+  
+    if (humanSequence.length === sequence.length) {
+      if (humanSequence.length === 15) {
+        resetGame('Congrats! You completed all the levels');
+        return
+}
+  
+    humanSequence = [];
+    info.textContent = 'Success! Keep going!';
+    setTimeout(() => {
+        nextRound();
+    }, 1000);
+    return;
+    }
+  
+    info.textContent = `Your turn: ${remainingTaps} Tap${
+      remainingTaps > 1 ? 's' : ''
+    }`;
+}
+    
+function startGame() {
+    startButton.classList.add('hidden');
+    info.classList.remove('hidden');
+    info.textContent = 'Wait for the computer';
+    nextRound();
+}
+
+startButton.addEventListener('click', startGame);
+boardContainer.addEventListener('click', event => {
+  const { card } = event.target.dataset;
+
+  if (tile) handleClick(tile);
+});
