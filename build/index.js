6be8c8cae8ba640882b271eca2e27d25a04ee915
Yulia <yuyarozental@gmail.com>
2022-12-27 09:45:46 -0500

    styling

--- build/index.js
+++ build/index.js
@@ -1,15 +1,13 @@
+"use strict";
 //array to keep track of the original sequence of tile clicks and a second array for the player sequence
 let sequence = [];
 let humanSequence = [];
-
 //to keep track of the numbers of rounds that have been played
 let level = 0;
-
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 const heading = document.querySelector('.js-heading');
 const boardContainer = document.querySelector('.js-container');
-
 function resetGame(text) {
     alert(text);
     sequence = [];
@@ -20,95 +18,74 @@ function resetGame(text) {
     info.classList.add('hidden');
     boardContainer.classList.add('unclickable');
 }
-  
 function humanTurn(level) {
     boardContainer.classList.remove('unclickable');
     info.textContent = `Your turn: ${level} Tap${level > 1 ? 's' : ''}`;
 }
-
 function activateCard(color) {
     const card = document.querySelector(`[data-card='${color}']`);
     const sound = document.querySelector(`[data-sound='${color}']`);
-
     card.classList.add('activated');
     sound.play();
-
     setTimeout(() => {
-    card.classList.remove('activated');
+        card.classList.remove('activated');
     }, 300);
 }
-  
 function playRound(nextSequence) {
     nextSequence.forEach((color, index) => {
-      setTimeout(() => {
-        activateCard(color);
-      }, (index + 1) * 600);
+        setTimeout(() => {
+            activateCard(color);
+        }, (index + 1) * 600);
     });
 }
-
 function nextStep() {
     const cards = ['pink', 'teal', 'orange', 'yellow', 'red', 'blue'];
     const random = cards[Math.floor(Math.random() * cards.length)];
     return random;
 }
-
 function nextRound() {
     level += 1;
-
     boardContainer.classList.add('unclickable');
     info.textContent = 'Wait for the computer';
     heading.textContent = `Level ${level} of 15`;
-
     const nextSequence = [...sequence];
     nextSequence.push(nextStep());
     playRound(nextSequence);
-
     sequence = [...nextSequence];
     setTimeout(() => {
-    humanTurn(level);
-  }, level * 600 + 1000);
+        humanTurn(level);
+    }, level * 600 + 1000);
 }
-
-
 function handleClick(tile) {
     const index = humanSequence.push(tile) - 1;
-  
     const remainingTaps = sequence.length - humanSequence.length;
-  
     if (humanSequence[index] !== sequence[index]) {
-      resetGame('Oops! Game over, you pressed the wrong tile');
-      return;
+        resetGame('Oops! Game over, you pressed the wrong tile');
+        return;
     }
-  
     if (humanSequence.length === sequence.length) {
-      if (humanSequence.length === 15) {
-        resetGame('Congrats! You completed all the levels');
-        return
-}
-  
-    humanSequence = [];
-    info.textContent = 'Success! Keep going!';
-    setTimeout(() => {
-        nextRound();
-    }, 1000);
-    return;
+        if (humanSequence.length === 15) {
+            resetGame('Congrats! You completed all the levels');
+            return;
+        }
+        humanSequence = [];
+        info.textContent = 'Success! Keep going!';
+        setTimeout(() => {
+            nextRound();
+        }, 1000);
+        return;
     }
-  
-    info.textContent = `Your turn: ${remainingTaps} Tap${
-      remainingTaps > 1 ? 's' : ''
-    }`;
+    info.textContent = `Your turn: ${remainingTaps} Tap${remainingTaps > 1 ? 's' : ''}`;
 }
-    
 function startGame() {
     startButton.classList.add('hidden');
     info.classList.remove('hidden');
     info.textContent = 'Wait for the computer';
     nextRound();
 }
-
 startButton.addEventListener('click', startGame);
 boardContainer.addEventListener('click', event => {
-  const { card } = event.target.dataset;
-
-  if (card) handleClick(card);
+    const { card } = event.target.dataset;
+    if (card)
+        handleClick(card);
 });
